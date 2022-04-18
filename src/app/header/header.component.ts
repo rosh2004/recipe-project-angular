2fc04fd27d09889afcddd40b3b1a91e4fa4ebba2
rosh_siddiqui <roshaan.siddiqui@techlogix.com>
2022-04-18 07:55:15 +0500

    complted the course project section -Basic

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  collapsed = true;
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
 }
