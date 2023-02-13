77133b88a97c97bde387fd9610d8e0f8d7287601
ROSHAANAHMED\Admin <roshaan.siddiqui@techlogix.com>
2023-02-14 00:01:46 +0500

    fixed effects with createEffects

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from "@angular/common/http";
-import { Actions, Effect, ofType } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { map, switchMap, tap, withLatestFrom } from "rxjs/operators";
 import { Recipe } from "../recipe.model";
 import * as fromApp from '../../store/app.reducer'
@@ -10,34 +10,34 @@ import { Router } from "@angular/router";
 
 @Injectable()
 export class RecipesEffects {
-
-  @Effect()
-  fetchRecipes = this.actions$.pipe(
-    ofType(RecipesActions.FETCH_RECIPES),
-    switchMap(() => {
-      let url = 'https://ng-udemy-9406c-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json'
-      return this.http.get<Recipe[]>(url)        
-    }),
-    map(recipes => {
-      return recipes.map(recipe => {
-        return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
-      })
-    }), map(recipes => {
-      // this.recipeService.setRecipes(recipes);
-      return new RecipesActions.SetRecipes(recipes);
-    }))
-
-    @Effect({dispatch: false})
-    storeRecipes = this.actions$.pipe(
-      ofType(RecipesActions.STORE_RECIPES),
-      withLatestFrom(this.store.select('recipes')),
-      switchMap(([actionType, recipesState])=>{
+  fetchRecipes = createEffect(()=>{
+    return this.actions$.pipe(
+      ofType(RecipesActions.FETCH_RECIPES),
+      switchMap(() => {
         let url = 'https://ng-udemy-9406c-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json'
-        return this.http.put(url, recipesState.recipes)
-         
-        
-      })
-    )
+        return this.http.get<Recipe[]>(url)        
+      }),
+      map(recipes => {
+        return recipes.map(recipe => {
+          return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
+        })
+      }), map(recipes => {
+        // this.recipeService.setRecipes(recipes);
+        return new RecipesActions.SetRecipes(recipes);
+      }))
+  })
+
+    
+    storeRecipes = createEffect(()=>{
+      return this.actions$.pipe(
+        ofType(RecipesActions.STORE_RECIPES),
+        withLatestFrom(this.store.select('recipes')),
+        switchMap(([actionType, recipesState])=>{
+          let url = 'https://ng-udemy-9406c-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json'
+          return this.http.put(url, recipesState.recipes)
+        })
+      )
+    }, {dispatch: false})
 
   constructor(private actions$: Actions,  private http: HttpClient, private store: Store<fromApp.AppState>){}
 }
